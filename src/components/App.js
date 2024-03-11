185f1abf70d34972531830b3da82d6a064c8369d
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-11 22:13:09 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,22 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para,setPara]=useState("")
   return (
     <div id="main">
-      // Do not alter the main div
-    </div>
+      
+      {para && <p id="para">{para}</p>}
+      <button
+        id="click"
+        onClick={() =>
+          setPara(
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
+          )
+        }
+      >
+        Click
+      </button>
+      </div>
   );
 }
 
